8488d60359bcd44e76e7d7598ef965bd2993d1de
werdani <ammaryasser554zz@gmail.com>
2021-03-30 21:12:21 +0200

    add views and templates

--- src/shop/models.py
+++ src/shop/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
  	name = models.CharField(max_length=200,db_index=True)
  	slug = models.SlugField(max_length=200,unique=True)
+
+
+ 	def get_absolute_url(self):
+ 		return reverse('shop:product_list_by_category',args=[self.slug])
+
  	class Meta:
  		ordering = ('name',)
  		verbose_name = 'category'
@@ -12,6 +18,7 @@ class Category(models.Model):
  	def __str__(self):
  		return self.name
 
+ 	
 class Product(models.Model):
  	category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
  	name  = models.CharField(max_length=200, db_index=True)
@@ -22,10 +29,14 @@ class Product(models.Model):
  	available = models.BooleanField(default=True)
  	created = models.DateTimeField(auto_now_add=True)
  	updated = models.DateTimeField(auto_now=True)
+ 	
+ 	def get_absolute_url(self):
+ 		return reverse('shop:product_detail',args=[self.id, self.slug])
 
  	class Meta:
  		ordering = ('name',)
  		index_together = (('id', 'slug'),)
- 		
+
  	def __str__(self):
  		return self.name
+ 	
