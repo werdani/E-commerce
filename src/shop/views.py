ebb682be0d66596b2af750b68753560233e7dbd0
werdani <ammaryasser554zz@gmail.com>
2021-04-06 01:03:18 +0200

    create application coupons

--- src/shop/views.py
+++ src/shop/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from django.core.paginator import Paginator
+
 
 
 def product_list(request, category_slug=None):
  	category = None
  	categories = Category.objects.all()
+	#product_list = Product.objects.all()
  	products = Product.objects.filter(available=True)
+	
  	if category_slug:
  		category = get_object_or_404(Category, slug=category_slug)
  		products = products.filter(category=category)
- 	return render(request,'product/list.html',
- 		           { 'category': category,
- 					 'categories': categories,
- 					 'products': products})
+
+	#paginator = Paginator(product_list, 3)
+	#page_number = request.GET.get('page')
+	#product_list = paginator.get_page(page_number)
+ 	return render(request,'product/list.html',{'category': category,
+												'categories': categories,
+												'products': products })
 
 
 def product_detail(request, id, slug):
